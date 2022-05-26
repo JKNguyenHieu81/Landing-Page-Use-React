83a3e05a2a4d3aa7c2e2dd5e125d4b4406d33422
hieuhulk <hulkgk12082002@gmail.com>
2022-05-26 13:20:50 +0700

    fix display for create section

--- src/SectionStore.js
+++ src/SectionStore.js
@@ -1,20 +1,19 @@
-//img: describeFirst.jpg
 import { Component } from "react";
 import describeFirst from "./img/describeFirst.jpg";
 class SectionStore extends Component {
   render() {
     return (
-      <div className="content-section create-section">
+      <div className="create-section">
         <div className="img-content">
           <img className="describe-first" alt="img" src={describeFirst} />
         </div>
-        <SectionText
-          subTitleText="Build your business"
-          titleText="Create Your Store"
-          descriptionText="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-erat, sed diam"
-        />
+        <div className="text-content-create-section">
+          <p className="sub-title-text">Build your business</p>
+          <p className="title-text">Create Your Store</p>
+          <p className="description-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam</p>
+        </div>
       </div>
     );
   }
